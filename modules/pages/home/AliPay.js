89c178a27cd8bf3c5ebdcfd1432b14e4384a64b6
yang657850144 <yang657850144@gmail.com>
2020-03-17 21:53:17 +0800

    feat: 网络接口请求

--- modules/pages/home/AliPay.js
+++ modules/pages/home/AliPay.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 
-function AliPayScreen({setting}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>收支明细: 当前余额 ${setting.count}</Text>
-    </View>
-  );
+class AliPayScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+    this.getData()
+  }
+  getData() {
+    console.log('发起请求')
+    fetch('https://yesno.wtf/api')
+      .then(res=>{
+        if(res.ok) {
+          return res.text()
+        }
+        throw new Error("Network Error", res)
+      })
+      .then(res=>{
+        res = JSON.parse(res)
+        this.setState({
+          count: res.answer
+        })
+      })
+  }
+  render() {
+    const {setting} = this.props
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>收支明细: 当前余额 ${this.state.count}</Text>
+      </View>
+    );
+
+  }
 }
 export default connect(state => ({
     setting: state.setting
